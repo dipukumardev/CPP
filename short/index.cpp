ee9f9b6a97b5eeee0759f1128b19a58d4b61986c
dipu kumar <dipukumardevcod@email.com>
2024-03-20 19:18:37 +0530

    make a question of selection sort

--- short/index.cpp
+++ short/index.cpp
@@ -1,7 +1,29 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int arr[5] ={ 1,5,2,4,32};
-    sort(arr.begin(), arr.end());
-    cout<<arr[5]<<endl;
+int so(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        int ans = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[ans])
+            {
+                ans = j;
+            }
+            if (ans != i) // Swap only if the minimum element is not at its correct position
+            {
+                swap(arr[ans], arr[i]);
+            }
+        }
+    }
+}
+int main()
+{
+    int arr[5] = {3, 2, 5, 4, 1};
+    so(arr,5);
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
 }
