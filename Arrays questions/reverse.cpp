8d34f5ae805451dac4b6aba2ee4331368611e007
dipu kumar <dipukumardevcod@email.com>
2024-03-28 11:57:42 +0530

    makin reverse and stl && leetcode questions

--- Arrays questions/reverse.cpp
+++ Arrays questions/reverse.cpp
@@ -1,5 +1,37 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    cout<<"dipu kumar  ";
+
+vector<int> reverse(vector<int> v)
+{
+    int start = 0, end = v.size() - 1;
+    while (start <= end)
+    {
+        swap(v[start], v[end]);
+        start++;
+        end--;
+    }
+    return v;
+}
+
+void print(vector<int> v){
+    for(int i =0; i<=v.size()-1; i++){
+        cout<<v[i] <<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    vector<int> v;
+    v.push_back(11);
+    v.push_back(91);
+    v.push_back(81);
+    v.push_back(9);
+    v.push_back(10);
+    v.push_back(1);
+
+    vector<int> ans = reverse(v);
+    print(ans);
+    return 0;
 }
---
+++ STL/list.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<list>
+using namespace std;
+int main(){
+
+    
+}
