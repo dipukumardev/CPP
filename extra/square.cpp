52364c81b344935c3d166a19ef6f7a7b68ce2b08
dipu kumar <dipukumardevcod@email.com>
2024-06-19 19:00:44 +0530

    find the febonacchi number of a number:

--- extra/square.cpp
+++ extra/square.cpp
@@ -39,3 +39,31 @@ int main()
         }
     }
 }
+
+
+
+//  find the fibonacchi numerb;
+#include<iostream>
+using namespace std;
+
+int fib(int n)
+{
+    if(n==0){
+        return 0;
+    }
+    if(n==1)
+    {
+        return 1;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
+
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of the n:";
+    cin>>n;
+    cout<<fib(n);
+}
