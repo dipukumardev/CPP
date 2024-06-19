0f647ed692bac7f52b6ad21f8cbe292e29fb50f6
dipu kumar <dipukumardevcod@email.com>
2024-06-19 18:59:25 +0530

    find the square of 1 to n number

---
+++ extra/square.cpp
@@ -0,0 +1,41 @@
+/*
+
+// start of the code;
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the value of the n:";
+    cin>>n;
+   for(int i = 1; i<=n; i++)
+   {
+    cout<< i <<" Square is = " <<i*i <<endl;
+   }
+}
+
+// end of the code;
+
+*/
+
+
+// when enter the n to find the fibonachi seris;
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n; 
+    cout<<"Enter the Value of the n:";
+    cin>>n;
+    for(int i = 1; i<=n; i++)
+    {
+        for(int j = 1; j<=i; j++)
+
+        {
+            // cout<<j * j<<endl;
+            
+        }
+    }
+}
