265b6fbe3517bffad56825f801cb505101e0b37f
dipu kumar <dipukumardevcod@email.com>
2024-07-22 23:18:15 +0530

    solve the rotate array in best solutions:

--- Rotate/Rotate_brute.cpp
+++ Rotate/Rotate_brute.cpp
@@ -1,55 +1,93 @@
-#include <iostream>
+// #include <iostream>
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // making a fundtion that rotate the arrays;
+
+// void leftrotate(int arr[], int n, int d)
+// {
+//     d = d % n;
+
+//     // make a tmep arrays;
+//     int temp[d];
+//     for (int i = 0; i < d; i++)
+//     {
+//         temp[i] = arr[i];
+//     }
+
+//     // a head the back elements;
+//     for (int i = d; i < n; i++)
+//     {
+//         arr[i - d] = arr[i];
+//     }
+
+//     // then puting the temp elm in the last;
+//     for (int i = n - d; i < n; i++)
+//     {
+//         arr[i] = temp[i - (n - d)];
+//     }
+// }
+// int main()
+// {
+//     // get the how many elements present the arrays;
+//     int n;
+//     cout << "Enter the element of the n:";
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     // how many elment put in the left;
+//     int d;
+//     cout << "Enter the elments of the d:";
+//     cin >> d;
+//     leftrotate(arr, n, d);
+
+//     // printing the arrayswhen remove the arrays;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+// solvig the same question with the help of class and best solutions;
+
 #include <bits/stdc++.h>
 using namespace std;
 
-// making a fundtion that rotate the arrays;
-
-void leftrotate(int arr[], int n, int d)
+class Solution
 {
-    d = d % n;
-
-    // make a tmep arrays;
-    int temp[d];
-    for (int i = 0; i < d; i++)
+public:
+    void rotate(int arr[], int n, int d)
     {
-        temp[i] = arr[i];
+        reverse(arr, arr + d);
+        reverse(arr + d, arr + n);
+        reverse(arr, arr + n);
     }
-
-    // a head the back elements;
-    for (int i = d; i < n; i++)
-    {
-        arr[i - d] = arr[i];
-    }
-
-    // then puting the temp elm in the last;
-    for (int i = n - d; i < n; i++)
-    {
-        arr[i] = temp[i - (n - d)];
-    }
-}
+};
 int main()
 {
-    // get the how many elements present the arrays;
-    int n;
-    cout << "Enter the element of the n:";
+    int n, d;
+    cout << "Enter the value of the n :";
     cin >> n;
     int arr[n];
+    cout << "Entet the number how elements are rotate:";
+    cin>>d;
+    // Enter the elements of the arrays:
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
+    Solution sol;
+    sol.rotate(arr, n, d);
 
-    // how many elment put in the left;
-    int d;
-    cout << "Enter the elments of the d:";
-    cin >> d;
-    leftrotate(arr, n, d);
-
-    // printing the arrayswhen remove the arrays;
+    // print the arrays;
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
-
-    return 0;
 }
