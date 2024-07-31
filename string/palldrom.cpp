cac51b08ed5e6b1227483b911fae7e5489654773
dipu kumar <dipukumardevcod@email.com>
2024-07-31 09:52:54 +0530

    second way to find the palldrome string

--- string/palldrom.cpp
+++ string/palldrom.cpp
@@ -1,19 +1,50 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     string d = "dipu";
+//     int start = 0;
+//     int end = d.size() -1;
+//     while(start<end)
+//     {
+//         if(d[start] != d[end])
+//         {
+//             cout<<"not a palldrom";
+//             return 0;
+//         }
+//        start++;
+//        end--;
+//     }
+//     cout<<"is a palldrom string";
+// }
+
+
+
+
+
+// second way to find the palldrom string:
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    string d = "dipu";
+    string s;
+    cout<<"Enter the s string:";
+    getline(cin, s);
+    int n = s.size() -1;
     int start = 0;
-    int end = d.size() -1;
+    int end = n;
     while(start<end)
+    
     {
-        if(d[start] != d[end])
+        if(s[start] != s[end])
         {
-            cout<<"not a palldrom";
+            cout<<"Not palldrome string;";
             return 0;
         }
-       start++;
-       end--;
+        start++;
+        end--;
     }
-    cout<<"is a palldrom string";
+    
+    cout<<"palldrome string:";
 }
