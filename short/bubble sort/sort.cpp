fa4194a655d51d4efa96029c81f54022a4ddfe66
dipu kumar <dipukumardevcod@email.com>
2024-08-27 05:32:02 +0530

    Sort an Arrays with the help of Bubble sort

---
+++ short/bubble sort/sort.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the Number:";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int counter = 1;
+    while (counter < n)
+    {
+        for (int i = 0; i < n - counter; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        counter++;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
