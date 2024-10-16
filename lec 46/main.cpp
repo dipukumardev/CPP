31661b832f14c65db62a2bb6d1279fa546e50a8f
dipu kumar <dipukumardevcod@email.com>
2024-10-16 19:36:00 +0530

    Second way to Address find

--- lec 46/main.cpp
+++ lec 46/main.cpp
@@ -58,3 +58,17 @@ int main()
         cout<<ptr << " ";
     }
 }
+
+// second way to find the Address of the Arrays;
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[5] = {1,3,4,6,7};
+   
+    for(int i =0; i<5; i++)
+    {
+        cout<<arr + i <<" ";
+    }
+}
