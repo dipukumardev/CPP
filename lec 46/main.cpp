8b2d2fecc3284bbff021a434259b1051227d7421
dipu kumar <dipukumardevcod@email.com>
2024-10-16 19:06:20 +0530

    how to store the address of the Arrays

---
+++ lec 46/main.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[5] = {1,3,4,56,5};
+    cout<<arr + 1; // formula use : ith index address = Base Address +  i * datasize(int,char);
+}
