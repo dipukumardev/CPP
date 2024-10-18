c6e1ae7aeeaff91dc8d121ebdd8200578106bd1e
dipu kumar <dipukumardevcod@email.com>
2024-10-18 10:20:45 +0530

    lec 47 1st qustions

---
+++ lec 47/main.cpp
@@ -0,0 +1,11 @@
+// print the ptr and arrays of c than see what is the answer;
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char c[6] = "abcde";
+    char *ptr = c;
+    cout<<c <<endl;
+    cout<<ptr;
+}
