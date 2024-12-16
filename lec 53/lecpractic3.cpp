001bd29390b88823dc565188f00d33ddd6be4499
dipu kumar <dipukumardevcod@email.com>
2024-12-16 21:49:25 +0530

    find the Power of the 2 si the nth number

--- lec 53/lecpractic3.cpp
+++ lec 53/lecpractic3.cpp
@@ -1,2 +1,24 @@
-// Calculate the power of 2n;
+// Calculate the power of 2 is the nth number;
+
+#include<iostream>
+using namespace std;
+
+
+int pow(int num, int n)
+{
+    if(n == 1)
+    {
+        return num;
+    }
+    
+    
+    return num * pow(num, n-1);
+}
+int main()
+{
+    int n;
+    cout<<"Enter the value of the N:";
+    cin>>n;
+    cout<<pow(2,n);
+}
 
