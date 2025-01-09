1ee46d33103edf218fc8c36fdad0a8c5d01a151b
dipu kumar <dipukumardevcod@email.com>
2025-01-09 11:14:16 +0530

    better approch

--- lec 17/1st.cpp
+++ lec 17/1st.cpp
@@ -1,21 +1,46 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int arr[6] = {1,2,4,6,4,32};
+//     int k;
+//     cout<<"Enter the value of the K:";
+//     cin>>k;
+
+//     for(int i =0; i<6; i++)
+//     {
+//         if(arr[i] == k)
+//         {
+//             cout<<"K is the match and position in Arrays is : " << i;
+//             return 0;
+//         }
+//     }
+//     cout<<"Not Match the value of the K in the Arrays:";
+
+//     return 0;
+// }
+
+// Second way to solve it
+
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int arr[6] = {1,2,4,6,4,32};
+    int arr[6] = {1, 2, 4, 6, 4, 32};
     int k;
-    cout<<"Enter the value of the K:";
-    cin>>k;
-    
-    for(int i =0; i<6; i++)
+    cout << "Enter the value of the K:";
+    cin >> k;
+    int index = -1;
+
+    for (int i = 0; i < 6; i++)
     {
-        if(arr[i] == k)
+        if (arr[i] == k)
         {
-            cout<<"K is the match and position in Arrays is : " << i;
-            return 0;
+            index = i;
+            break;
         }
     }
-    cout<<"Not Match the value of the K in the Arrays:";
-    
+    cout << index;
+
     return 0;
 }
