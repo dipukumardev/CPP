9d7471895e349f03edc3dfac0896da2a12b62c8b
dipu kumar <dipukumardevcod@email.com>
2025-01-09 18:30:16 +0530

    Reverse Arrays

---
+++ lec 17/2nd.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void reverse(int arr[], int size)
+{
+    int start = 0;
+    int end = size - 1;
+
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    reverse(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
