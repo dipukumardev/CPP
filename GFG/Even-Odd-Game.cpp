9458d67730aba746470b03947ad8ace11dacc94f
Dipu kumar <dipukumardevcod@gmail.com>
2025-06-19 23:12:05 +0530

    Even odd

---
+++ GFG/Even-Odd-Game.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the value of the N:";
+    cin>>n;
+    if(n%2 == 0)
+    {
+        cout<<"Dipu Win";
+    }
+   else{
+       cout<<"Friends wins";
+   }
+}
